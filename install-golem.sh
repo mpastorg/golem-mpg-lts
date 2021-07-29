674eda7073493416d598a9e39a18192b6b509254
mpastorg <marcos@pastorg.es>
2021-07-29 05:01:33 -0500

    Update install-golem.sh

--- install-golem.sh
+++ install-golem.sh
@@ -9,8 +9,4 @@ echo 'COMPOSE_PROJECT_NAME=golem-'$exec_datetime >> .env
 echo 'YA_PAYMENT_NETWORK=mainnet' >> .env
 echo 'YA_ACCOUNT=0x6A6e231bA285DdBa76C2E866Bc35446025cCd63a' >> .env
 cp .env .env.local
-#printf '\n\n\n\n' | make setup &
-printf '\n\n\n\n' | docker-compose run -d node golemsp run
-sleep 90
-#make up
-
+printf '\n\n\n\n' | make setup
